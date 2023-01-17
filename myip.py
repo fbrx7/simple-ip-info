9b215b09d38df74340263cb615dcd4de54701288
z3kan <mr.r0b0t0x17@gmail.com>
2023-01-17 22:28:40 +0300

    Some simple updates in script

--- myip.py
+++ myip.py
@@ -7,14 +7,14 @@ class Main:
 	url = 'https://ipinfo.io/json'
 
 	def __init__(self):
-		self.response = req.get(Main.url)
-		self.ip = self.response.json()['ip']
-		self.city = self.response.json()['city']
-		self.region = self.response.json()['region']
-		self.country = self.response.json()['country']
-		self.loc = self.response.json()['loc']
-		self.org = self.response.json()['org']
-		self.timezone = self.response.json()['timezone']
+			self.response = req.get(Main.url)
+			self.ip = self.response.json()['ip']
+			self.city = self.response.json()['city']
+			self.region = self.response.json()['region']
+			self.country = self.response.json()['country']
+			self.loc = self.response.json()['loc']
+			self.org = self.response.json()['org']
+			self.timezone = self.response.json()['timezone']
 
 	def display(self):
 		print (f'{Fore.LIGHTBLUE_EX}IP : {Fore.LIGHTRED_EX}{self.ip}')
@@ -25,4 +25,10 @@ class Main:
 		print (f'{Fore.LIGHTBLUE_EX}org : {Fore.LIGHTRED_EX}{self.org}')
 		print (f'{Fore.LIGHTBLUE_EX}timezone : {Fore.LIGHTRED_EX}{self.timezone}')
 
-op1 = Main().display()
+try:
+
+	op1 = Main().display()
+
+except:
+
+	print (f'{Fore.LIGHTRED_EX}There is a error, Please check your network connection...')
